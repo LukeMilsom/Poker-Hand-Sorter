fc46d0b1c2d470a2cde9fa6af2083be4b61bacbd
LukeMilsom <69194778+LukeMilsom@users.noreply.github.com>
2021-07-13 16:53:09 +1000

    Removed redundant logic

--- utils/combinations/isHighCard.mjs
+++ utils/combinations/isHighCard.mjs
@@ -1,9 +1,3 @@
-import { ALL_NUMBERS_IN_ORDER_OF_VALUE } from "../../constants/combinations.mjs";
-import { sortByCardNumber } from "../commonUtilities.mjs";
-
-export const isHighCard = (cardNumbers) => {
-  const referenceArray = ALL_NUMBERS_IN_ORDER_OF_VALUE;
-  const sortedArray = sortByCardNumber(cardNumbers, referenceArray);
-
+export const isHighCard = () => {
   return true;
 };
