68ff1184630e873b68d26b6eb6b8a73f2c58b43d
LukeMilsom <69194778+LukeMilsom@users.noreply.github.com>
2021-07-13 16:51:27 +1000

    Removed comment

--- utils/combinations/isHighCard.mjs
+++ utils/combinations/isHighCard.mjs
@@ -5,7 +5,5 @@ export const isHighCard = (cardNumbers) => {
   const referenceArray = ALL_NUMBERS_IN_ORDER_OF_VALUE;
   const sortedArray = sortByCardNumber(cardNumbers, referenceArray);
 
-  //be careful this doesnt return falsey
-  //return sortedArray.slice(-1)[0];
   return true;
 };
